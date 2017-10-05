c11afda45de7e3b21f115a988df349e6bc860989
deeptostring <deeptostring@gmail.com>
2017-10-05 11:52:34 +0300

    "mod classes "

--- courses-java/src/Fibonacci.java
+++ courses-java/src/Fibonacci.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Fibonacci {
+    public static void main(String[] args) {
+        int lo = 1;
+        int hi = 1;
+        System.out.println(lo);
+        while (hi < 50){
+            System.out.println(hi);
+            hi = lo + hi;
+            lo = hi - lo;
+        }
+    }
 }
